93bb3a3b03958af815bf09b9c0cf2a91182cdc16
angelpb0904 <angelpb0904@gmail.com>
2021-05-30 03:45:29 -0400

    Primer servidor con express

---
+++ express/servidor.js
@@ -0,0 +1,19 @@
+var cafecito = require("express");
+var aplicacion = cafecito();
+
+aplicacion.get("/", inicio);
+aplicacion.get("/cursos", cursos);
+
+function inicio(peticion, resultado)
+{
+  resultado.send("Este es el <strong>home</strong> genial!!");
+}
+
+function cursos(peticion, resultado)
+{
+  resultado.send("Estos son los <strong>cursos</strong>");
+}
+
+aplicacion.listen(8989);
+
+// platzi.com/js
